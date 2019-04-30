dedec28c0b40fdbec70a7efa3e3d55e37b17e0e9
yunyoung1819 <yunyoung1819@hanmai.net>
2019-04-30 17:44:04 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,76 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import nkia from './nkia.png';
+import './App.scss';
+import TodoHeader from "./TodoHeader";
+import TodoContents from "./TodoContents";
+import todoService from "./service/todoService"
 
 class App extends Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      myMessage : '',
+      list : [],
+      checkNum : 0
+    }
+  }
+
+  componentDidMount() {
+    this.getTodoList()
+  }
+
+  getTodoList = async () => {
+    try {
+      const list = await todoService.getTodoList()
+      this.setState({
+        list
+      })
+    } catch (e) {
+      console.error(e)
+    }
+  }
+
+  inputValue(value) {
+    this.setState({
+      myMessage: value
+      }
+    )
+  }
+  addInputValue(value) {
+    this.setState((state) => {
+      return {
+        list: state.list.concat(value),
+        myMessage: ''
+      }
+    });
+  }
+  handleTodoRemove = (val) => {
+    this.setState((state) => {
+      return {
+        list : state.list.filter(todo => todo !== val)
+      }
+    });
+  }
+  handleTodoComple= (val) => {
+    this.setState((state) => {
+      return {
+        list : state.list.filter(todo => todo !== val),
+        checkNum : state.checkNum + 1
+      }
+    })
+  }
+  // arrowFunction = (인자) => 반환
+  handleTodoChange = (e) => this.inputValue(e.target.value)
+
+  handleTodoAdd = () => this.addInputValue(this.state.myMessage)
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="todo_list">
+        <TodoHeader logo={nkia} todoText={this.state.myMessage} onTodoChange={this.handleTodoChange} onTodoAdd={this.handleTodoAdd} />
+        <TodoContents todoItems={this.state.list} onTodoComplete={this.handleTodoComplete} onTodoRemove={this.handleTodoRemove} completeCount={this.state.checkNum} />
       </div>
     );
   }
---
+++ src/TodoHeader.js
@@ -0,0 +1,30 @@
+import React, {Component} from 'react';
+
+class TodoHeader extends Component {
+    shouldComponentUpdate(nextProps) {
+        if (this.props.todoText !== nextProps.todoText) return true;
+        return false;
+    }
+    render() {
+        const {
+            logo,
+            todoText,
+            onTodoChange,
+            onTodoAdd,
+        } = this.props;
+        return (
+            <div className="todo_header">
+                <img src={logo} className="App-logo" alt="logo" />
+                <input
+                    type="text"
+                    className="todo_input"
+                    value={todoText}
+                    onChange={onTodoChange}
+                />
+                <button className="todo_button" onClick={onTodoAdd}> </button>
+            </div>
+        );
+    }
+}
+
+export default TodoHeader;
